bab36b3c9bd104bfc097c421d0bbfc5f171afe6c
forget-feature <1915144429@qq.com>
2022-01-23 20:34:33 +0800

    初始化仓库

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  root: "./",
+  server: {
+    port: 928,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      "@": "./src",
+    },
+    extensions: [".js", ".ts", ".vue", ".json", ".css", "..."],
+  },
+})
