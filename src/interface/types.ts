0c691bab054f70bb8a9b8d58d0468f7d65cdd27e
wsyya <1915144429@qq.com>
2022-04-08 18:29:45 +0800

    types

--- src/interface/types.ts
+++ src/interface/types.ts
@@ -1,3 +1,6 @@
+// 按钮类型
 export type Types = "primary" | "default" | "danger" | "warning";
+// 圆角类型
 export type Rounded = "small" | "middle" | "large" | "none" | "circle";
+// 输入框类型
 export type TextType= "text" | "textarea" | "radio" | "checkbox"
