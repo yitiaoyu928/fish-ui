0b52796380b42b8a37b720bf1312f73e3eeda3c1
qigeyu <1915144429@qq.com>
2022-09-22 14:12:34 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -7,31 +7,32 @@
         <f-tag>hello</f-tag>
       </div>
       <div class="container-item">
-        <f-button type="primary" size="medium" plain>hello</f-button><f-button type="default" plain>hello</f-button>
-        <f-button type="warning" plain>hello</f-button>
-        <f-button type="danger" plain>hello</f-button>
+        <f-button type="primary" size="medium" plain>hello</f-button
+        ><f-button type="default" plain>hello</f-button
+        ><f-button type="warning" plain>hello</f-button
+        ><f-button type="danger" plain>hello</f-button>
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
-let loading = ref(false)
+import { ref } from "vue";
+let loading = ref(false);
 function handleClick() {
-  console.log(123)
-  loading.value = true
+  console.log(123);
+  loading.value = true;
   setTimeout(() => {
-    loading.value = false
-  }, 3000)
+    loading.value = false;
+  }, 3000);
 }
-let switchValue = ref(false)
+let switchValue = ref(false);
 function handleSwitchChange(e: boolean): void {
-  console.log(e)
-  switchValue.value = e
+  console.log(e);
+  switchValue.value = e;
 }
 </script>
 <style lang="scss" scoped>
-.container+.container {
+.container + .container {
   margin-bottom: 20px;
 }
 
@@ -39,7 +40,7 @@ function handleSwitchChange(e: boolean): void {
   border-bottom: 1px solid #ccc;
 }
 
-.container-item+.container-item {
+.container-item + .container-item {
   margin-bottom: 20px;
 }
 </style>
